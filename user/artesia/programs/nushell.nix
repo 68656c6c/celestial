dcbd4f6292237f25c151a2e050bf30d8f198fd36
68656c6c <dennis@artesia.moe>
2026-08-26 20:46:56 +0200

    nushell changes more

--- user/artesia/programs/nushell.nix
+++ user/artesia/programs/nushell.nix
@@ -1,33 +1,13 @@
-{ pkgs, config, osConfig, ... }:
+{
+  pkgs,
+  config,
+  osConfig,
+  ...
+}:
 
 {
   programs.nushell = {
     enable = true;
-    environmentVariables = {
-      NIXOS_OZONE_WL = 1;
-      NH_OS_FLAKE = "${config.home.homeDirectory}.dotfiles";
-      XCURSOR_SIZE = ${builtins.toString osConfig.host.cursor.size};
-      SSH_AUTH_SOCK = "${config.home.homeDirectory}.bitwarden-ssh-agent.sock";
-    };
-    configFile.text = ''
-      $env.config.show_banner = false
-      $env.config.buffer_editor = "hx"
-      $env.config.completions = {
-          case_sensitive: false
-          quick: true
-          partial: true
-          algorithm: "fuzzy"
-        }
-      $env.config.history = {
-        file_format: sqlite
-        max_size: 1_000_000
-        sync_on_enter: true
-        isolation: false
-      }
-    '';
-    extraConfig = ''
-      def nwhich [flag: string] { readlink -f (which ... $flag) }
-    '';
     shellAliases = {
       la = "eza -la";
       ll = "eza -l";
@@ -36,6 +16,31 @@
       lt = "eza --tree";
       tree = "eza --tree";
     };
+    environmentVariables = {
+      NIXOS_OZONE_WL = 1;
+      NH_OS_FLAKE = "${config.home.homeDirectory}/.dotfiles";
+      XCURSOR_SIZE = osConfig.host.cursor.size;
+      SSH_AUTH_SOCK = "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
+    };
+    settings = {
+      show_banner = false;
+      buffer_editor = "hx";
+      completions = {
+        case_sensitive = false;
+        quick = true;
+        partial = true;
+        algorithm = "fuzzy";
+      };
+      history = {
+        file_format = "sqlite";
+        max_size = 1000000;
+        sync_on_enter = true;
+        isolation = false;
+      };
+    };
+    extraConfig = ''
+      def nwhich [flag: string] { readlink -f (which ... $flag) }
+    '';
   };
 
   programs.carapace = {
